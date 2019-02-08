5895873126a60683b27d0e09b048fd50b44a2c6c
Ignacio Buioli <ibuioli@gmail.com>
2019-02-08 03:52:52 +0000

    feat: migration to odoo 12

--- models/models.py
+++ models/models.py
@@ -5,7 +5,6 @@ from odoo import models, fields, api
 class WebsiteSocial(models.Model):
 	_inherit = 'website'
 
-	social_instagram = fields.Char('Instagram Account')
 	social_bitbucket = fields.Char('BitBucket Account')
 	social_vimeo = fields.Char('Vimeo Account')
 	social_whatsapp = fields.Char('Whatsapp Number')
@@ -16,16 +15,15 @@ class WebsiteSocial(models.Model):
 	social_flickr = fields.Char('Flickr Account')
 
 class WebsiteSocialConfig(models.TransientModel):
-        _inherit = 'website.config.settings'
+	_inherit = 'res.config.settings'
 
-        social_instagram = fields.Char(related='website_id.social_instagram')
-        social_bitbucket = fields.Char(related='website_id.social_bitbucket')
-        social_vimeo = fields.Char(related='website_id.social_vimeo')
-        social_whatsapp = fields.Char(related='website_id.social_whatsapp')
-	social_skype = fields.Char(related='website_id.social_skype')
-        social_twitch = fields.Char(related='website_id.social_twitch')
-        social_reddit = fields.Char(related='website_id.social_reddit')
-        social_tumblr = fields.Char(related='website_id.social_tumblr')
-        social_flickr = fields.Char(related='website_id.social_flickr')
+	social_bitbucket = fields.Char(related='website_id.social_bitbucket', readonly=False)
+	social_vimeo = fields.Char(related='website_id.social_vimeo', readonly=False)
+	social_whatsapp = fields.Char(related='website_id.social_whatsapp', readonly=False)
+	social_skype = fields.Char(related='website_id.social_skype', readonly=False)
+	social_twitch = fields.Char(related='website_id.social_twitch', readonly=False)
+	social_reddit = fields.Char(related='website_id.social_reddit', readonly=False)
+	social_tumblr = fields.Char(related='website_id.social_tumblr', readonly=False)
+	social_flickr = fields.Char(related='website_id.social_flickr', readonly=False)
 
 
