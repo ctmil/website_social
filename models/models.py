3aeaf7043abfef52f8b1bfefdc6d15c1063786f6
Ignacio Buioli <ibuioli@gmail.com>
2019-05-28 18:08:57 +0000

    feat: add Pinterest and Houzz

--- models/models.py
+++ models/models.py
@@ -13,6 +13,8 @@ class WebsiteSocial(models.Model):
 	social_reddit = fields.Char('Reddit Account')
 	social_tumblr = fields.Char('Tumblr Account')
 	social_flickr = fields.Char('Flickr Account')
+	social_pinterest = fields.Char('Pinterest Account')
+	social_houzz = fields.Char('Houzz Account')
 
 class WebsiteSocialConfig(models.TransientModel):
 	_inherit = 'res.config.settings'
@@ -25,5 +27,6 @@ class WebsiteSocialConfig(models.TransientModel):
 	social_reddit = fields.Char(related='website_id.social_reddit', readonly=False)
 	social_tumblr = fields.Char(related='website_id.social_tumblr', readonly=False)
 	social_flickr = fields.Char(related='website_id.social_flickr', readonly=False)
-
+	social_pinterest = fields.Char(related='website_id.social_pinterest', readonly=False)
+	social_houzz = fields.Char(related='website_id.social_houzz', readonly=False)
 
